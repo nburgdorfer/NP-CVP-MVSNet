b5efcf7b9d3e567c2721af15990535a8daa65c7f
nburgdorfer <nburgdorfer@gmail.com>
2022-09-27 01:05:31 -0400

    updates

--- fusion/dtu_fusion.sh
+++ fusion/dtu_fusion.sh
@@ -14,16 +14,16 @@ EVAL_PC_DIR=${EVAL}mvs_data/Points/${METHOD}/
 EVAL_RESULTS_DIR=${EVAL}mvs_data/Results/
 
 fusion() {
-	#	python depthfusion.py \
-	#	--dtu_test_root=$DTU_TEST_ROOT \
-	#	--depth_folder=$DEPTH_FOLDER \
-	#	--scene=$1 \
-	#	--out_folder=$OUT_FOLDER \
-	#	--fusibile_exe_path=$FUSIBILE_EXE_PATH \
-	#	--prob_threshold=0.8 \
-	#	--disp_threshold=$2 \
-	#	--num_consistent=$3 \
-	#	--image_height=1200
+	python depthfusion.py \
+	--dtu_test_root=$DTU_TEST_ROOT \
+	--depth_folder=$DEPTH_FOLDER \
+	--scene=$1 \
+	--out_folder=$OUT_FOLDER \
+	--fusibile_exe_path=$FUSIBILE_EXE_PATH \
+	--prob_threshold=0.8 \
+	--disp_threshold=$2 \
+	--num_consistent=$3 \
+	--image_height=1200
 
 	# move merged point cloud to evaluation path
 	cp ${OUT_FOLDER}${1}consistencyCheck/final3d_model.ply ${EVAL_PC_DIR}${PC_FILE_NAME}
