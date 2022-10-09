a3baddc33902c99ed36e3c688f3b4a99c8c3afaa
nburgdorfer <nburgdorfer@gmail.com>
2022-10-09 15:38:54 -0400

    Running intermediate

--- fusion/tnt_fusion.sh
+++ fusion/tnt_fusion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATASET=training
-#DATASET=intermediate
+#DATASET=training
+DATASET=intermediate
 
 TRAINING_SET=dtu
 #TRAINING_SET=blended_mvs
@@ -19,21 +19,21 @@ fusion() {
 	DISP_TH=$2
 	NUM_CONS=$3
 
-	## compute the alignment for mvsnet -> GT
-    echo -e "\e[1;33mComputing scene alignmment...\e[0;37m"
-	python -u ../tools/alignment/compute_alignment.py \
-			-a ${DEPTH_FOLDER}${SCENE}/cam/ \
-			-b ${COLMAP_DIR}Cameras/${SCENE}/ \
-			-t ${COLMAP_DIR}Cameras/${SCENE}/alignment.txt \
-			-o ${DEPTH_FOLDER}${SCENE}/cam/alignment.txt
+	#	## compute the alignment for mvsnet -> GT
+    #	echo -e "\e[1;33mComputing scene alignmment...\e[0;37m"
+	#	python -u ../tools/alignment/compute_alignment.py \
+	#			-a ${DEPTH_FOLDER}${SCENE}/cam/ \
+	#			-b ${COLMAP_DIR}Cameras/${SCENE}/ \
+	#			-t ${COLMAP_DIR}Cameras/${SCENE}/alignment.txt \
+	#			-o ${DEPTH_FOLDER}${SCENE}/cam/alignment.txt
 
-	## convert mvsnet cameras into .log format (w/ alignment)
-    echo -e "\e[1;33mCreating tnt camera file...\e[0;37m"
-	python -u ../tools/conversion/convert_to_log.py \
-			-d ${DEPTH_FOLDER}${SCENE}/cam/ \
-			-f mvsnet \
-			-a ${DEPTH_FOLDER}${SCENE}/cam/identity_trans.txt \
-			-o ${DEPTH_FOLDER}${SCENE}/cam/camera_pose.log
+	#	## convert mvsnet cameras into .log format (w/ alignment)
+    #	echo -e "\e[1;33mCreating tnt camera file...\e[0;37m"
+	#	python -u ../tools/conversion/convert_to_log.py \
+	#			-d ${DEPTH_FOLDER}${SCENE}/cam/ \
+	#			-f mvsnet \
+	#			-a ${DEPTH_FOLDER}${SCENE}/cam/identity_trans.txt \
+	#			-o ${DEPTH_FOLDER}${SCENE}/cam/camera_pose.log
 
 	python depthfusion.py \
 	--dtu_test_root=$TNT_TEST_ROOT \
@@ -60,17 +60,21 @@ evaluate() {
 			--ply-path ${PLY}
 }
 
-SCENES=(Barn Caterpillar Ignatius Truck)
-DISP_TH=(0.35 0.20 0.55 0.30)
-NUM_CONSIST=(3 6 2 3)
+#SCENES=(Barn Caterpillar Ignatius Truck)
+#DISP_TH=(0.35 0.20 0.55 0.30)
+#NUM_CONSIST=(3 6 2 3)
+
+SCENES=(Family Francis Horse Lighthouse M60 Panther Playground Train)
+DISP_TH=(0.25 0.25 0.20 0.30 0.25 0.20 0.25 0.25)
+NUM_CONSIST=(4 4 7 5 4 4 5 5)
 i=0
 
 for SCENE in ${SCENES[@]}
 do
 	echo "Working on scene ${SCENE} ($i)"
 	fusion ${SCENE} ${DISP_TH[$i]} ${NUM_CONSIST[$i]}
-    evaluate $SCENE ${OUT_FOLDER}${SCENE}/consistencyCheck/final3d_model.ply &
-	wait
+    #evaluate $SCENE ${OUT_FOLDER}${SCENE}/consistencyCheck/final3d_model.ply &
+	#wait
 
 	let i=$i+1
 done
